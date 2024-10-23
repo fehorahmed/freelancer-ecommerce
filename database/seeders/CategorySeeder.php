09804bf21129de8f5e4028bb14703b91c1d94830
ahmedfehor <ahmedfehor@gmail.com>
2024-10-24 00:36:59 +0600

    ok

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,11 +11,11 @@ class CategorySeeder extends Seeder
 
     protected $categories = [
         [ 'id' => 1,'root_id' =>null, 'name' => 'Root Category', 'url' => 'root-category' ],
-        [ 'id' => 2,'root_id' =>1,'name' => 'Smart Watch', 'url' => 'smart-watch' ],
-        [ 'id' => 3,'root_id' =>1,'name' => 'Headphone', 'url' => 'headphone'],
-        [ 'id' => 4,'root_id' =>1,'name' => 'Smart TV', 'url' => 'smart-tv'],
-        [ 'id' => 5,'root_id' =>1,'name' => 'Life Style', 'url' => 'life-style'],
-        [ 'id' => 6,'root_id' =>1,'name' => 'Luggage & Bags', 'url' => 'luggage-and-bags'],
+        [ 'id' => 2,'root_id' =>1,'name' => 'Beef', 'url' => 'beef' ],
+        [ 'id' => 3,'root_id' =>1,'name' => 'Vegetables', 'url' => 'vegetables'],
+        // [ 'id' => 4,'root_id' =>1,'name' => 'Smart TV', 'url' => 'smart-tv'],
+        // [ 'id' => 5,'root_id' =>1,'name' => 'Life Style', 'url' => 'life-style'],
+        // [ 'id' => 6,'root_id' =>1,'name' => 'Luggage & Bags', 'url' => 'luggage-and-bags'],
     ];
     /**
      * Run the database seeds.
