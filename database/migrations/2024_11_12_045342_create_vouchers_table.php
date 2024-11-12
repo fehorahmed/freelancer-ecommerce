4a3a9ef0534611be906ade44a1da46328c4479ea
ahmedfehor <ahmedfehor@gmail.com>
2024-11-12 11:01:06 +0600

    ok

---
+++ database/migrations/2024_11_12_045342_create_vouchers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('coupon_code');
+            $table->integer('no_of_usage');
+            $table->double('discount_amount')->nullable();
+            $table->double('discount_percentage')->nullable();
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->date('end_date');
+            $table->time('end_time');
+            $table->tinyInteger('status')->default(1);
+            $table->boolean('visibility')->default(1);
+            $table->boolean('is_apps_only')->default(0);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('admins');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('admins');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
